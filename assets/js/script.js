2fd4f788873e79f210dc431d03ea66bb0b3b0744
Danielle Cavinder <daniellecavinder@Danielles-MBP.home>
2023-01-05 17:19:15 -0700

    made changes to js file and added gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,29 @@ function getApi() {
 }
 
 searchButton.addEventListener('click', getApi);
+var cityEl = document.getElementById('city-search');
+var searchButton = document.getElementById('search-button');
+
+function hideForecast() {
+    document.getElementById("current-forecast").hidden = true
+    document.getElementById("five-day-forecast").hidden = true
+}
+hideForecast()
+
+
+
+    var fiveDayForcast = document.createElement('div');
+    
+    var dayOne = document.createElement('div');
+    var dayTwo = document.createElement('div');
+    var dayThree = document.createElement('div');
+    var dayFour = document.createElement('div');
+    var dayFive = document.createElement('div');
+    fiveDayForcast.append(dayOne, dayTwo, dayThree, dayFour, dayFive);
+
+    //appending temps
+    dayOne.append(temp, wind, humidity);
+    dayTwo.append(temp, wind, humidity);
+    dayThree.append(temp, wind, humidity);
+    dayFour.append(temp, wind, humidity);
+    dayFive.append(temp, wind, humidity);
